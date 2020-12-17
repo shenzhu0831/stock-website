7dbd80e5af874ab00e348a5bc04906c9902bdc19
G100my <G100my.lan229@gmail.com>
2020-12-17 14:05:08 +0800

    feat(main): add ID for add listener, add parameter in createTable

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import getStockData from './scripts/search.js';
 import { createTitle, createBody } from './dynamicComponents/createSheet.js';
 import './scripts/checkIndent.js';
-import {reportYear2330} from './fake/data.js';
+import {reportYear2330, reportRatioYear2330, chartAssetYear2330} from './fake/data.js';
 
 const searchInput = document.getElementById('search-input');
 const searchInputIcon = document.getElementById('search-icon');
 const balanceSheetButton = document.getElementById('balance-sheet');
+const perShareRatiosButton = document.getElementById('per_share_ratios');
+const workingCapitalButton = document.getElementById('workingCapital');
 const displayArea = document.querySelector('section[class="data"]');
 
 let reportYear;
@@ -32,11 +34,11 @@ searchInputIcon.addEventListener('click', () => {
     searchInput.value = '';
 });
 
-function createTable(titlText, topText, incomeData) {
+function createTable(titlText, topText, incomeData, RowName) {
     const container = document.createElement('div');
     const title = createTitle(titlText);
     const body = createBody(topText, incomeData);
-    body.setRowName('balance_sheets');
+    body.setRowName(RowName);
     body.setCell(incomeData);
 
     container.append(title, body);
@@ -46,9 +48,32 @@ function createTable(titlText, topText, incomeData) {
 balanceSheetButton.addEventListener('click', function () {
     const balanceSheet = createTable(
         '資產負債表',
-        '',
-        reportYear2330.year_balance_sheets
+        '2330_台積電_資產負債表_年',
+        reportYear2330.year_balance_sheets,
+        'balance_sheets'
     );
     displayArea.textContent = '';
     displayArea.append(balanceSheet);
 });
+
+perShareRatiosButton.addEventListener('click', function () {
+    const balanceSheet = createTable(
+        '每股比例表',
+        '2330_台積電_資產負債表_年',
+        reportRatioYear2330.year_per_share_ratios,
+        'cash_flow_statements'
+    );
+    displayArea.textContent = '';
+    displayArea.append(balanceSheet);
+})
+
+workingCapitalButton.addEventListener('click', function () {
+    const balanceSheet = createTable(
+        '營運資金週期',
+        '2330_台積電_營運資金週期_年',
+        chartAssetYear2330.workingCapital,
+        'liquidity_analysis'
+    );
+    displayArea.textContent = '';
+    displayArea.append(balanceSheet);
+})
