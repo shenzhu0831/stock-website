31d8d65ccca96576c45e4a8452bb3e3130af1dd8
G100my <G100my.lan229@gmail.com>
2020-12-20 01:43:26 +0800

    feat(main): link to createTable.js, instead of createSheet.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import getStockData from './scripts/search.js';
-import { createTableBody } from './dynamicComponents/createSheet.js';
+import createTableBody from './dynamicComponents/createTable.js';
 import './scripts/checkIndent.js';
+import transformData from './scripts/transformData.js'
 
 const searchInput = document.getElementById('search-input');
 const searchInputIcon = document.getElementById('search-icon');
@@ -16,13 +17,23 @@ let reportYear = localStorage.getItem('reportYear') || undefined;
 let reportRatioYear = localStorage.getItem('reportRatioYear') || undefined;
 let chartAssetYear = localStorage.getItem('chartAssetYear') || undefined;
 let whichPage;
+let currentConpanyName;
+let lastSearchedStockCode;
+
+function afterGetStockDataHandler(data) {
+    ({ reportYear, reportRatioYear, chartAssetYear } = data);
+    currentConpanyName = reportYear.company_name;
+    reportYear = transformData(reportYear, 'year_balance_sheets');
+    reportRatioYear = transformData(reportRatioYear, 'year_per_share_ratios');
+    chartAssetYear = transformData(chartAssetYear, 'workingCapital');
+    reRender(whichPage);
+}
 
 function searchHandler() {
     let stockCode = searchInput.value;
     getStockData(stockCode)
         .then((data) => {
-            ({ reportYear, reportRatioYear, chartAssetYear } = data);
-            reRender(whichPage);
+            afterGetStockDataHandler(data);
             localStorage.setItem('lastSearchedStockCode', stockCode);
         })
         .catch((error) => console.error(error));
@@ -60,31 +71,31 @@ function reRender(whichPage) {
     switch (whichPage) {
         case 'balanceSheet':
             displayTitle.textContent = '資產負債表';
-            tableBody = createTableBody(reportYear.year_balance_sheets);
+            tableBody = createTableBody(reportYear);
             break;
         case 'perShareRations':
             displayTitle.textContent = '每股比例表';
-            tableBody = createTableBody(reportRatioYear.year_per_share_ratios);
+            tableBody = createTableBody(reportRatioYear);
             break;
         case 'workingCapital':
             displayTitle.textContent = '營運資金週期';
-            tableBody = createTableBody(chartAssetYear.workingCapital);
+            tableBody = createTableBody(chartAssetYear);
             break;
         default:
             throw 'can not match any page';
     }
-    formTitle.textContent = `${reportYear.stock_symbol}_${reportYear.company_name}_${displayTitle.textContent}_年`;
+    formTitle.textContent = `${lastSearchedStockCode}_${currentConpanyName}_${displayTitle.textContent}_年`;
     formContainer.textContent = '';
     formContainer.append(tableBody);
 }
 
 window.addEventListener('load', () => {
-    const lastSearchedStockCode = localStorage.getItem('lastSearchedStockCode') || '2330';
+    lastSearchedStockCode = localStorage.getItem('lastSearchedStockCode') || '2330';
     whichPage = localStorage.getItem('whichPage') || 'balanceSheet';
     getStockData(lastSearchedStockCode)
-        .then((data) => {
-            ({ reportYear, reportRatioYear, chartAssetYear } = data);
-            reRender(whichPage);
-        })
+    .then((data) => {
+        afterGetStockDataHandler(data)
+        reRender(whichPage);
+    })
         .catch((error) => console.error(error));
 });
